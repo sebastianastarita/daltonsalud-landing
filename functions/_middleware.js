2464a028dc29dc52a33050395b5fb1e31e92e937
Sebastian Astarita <sebastianastarita@gmail.com>
2026-06-05 16:42:40 -0300

    Redirect defensive domains to canonical host

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,16 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
+  const canonicalHost = "daltonsalud.com";
+  const redirectHosts = new Set([
+    "www.daltonsalud.com",
+    "daltonsalud.com.ar",
+    "www.daltonsalud.com.ar",
+    "farmaciadalton.com.ar",
+    "www.farmaciadalton.com.ar",
+  ]);
 
-  if (url.hostname === "www.daltonsalud.com") {
-    url.hostname = "daltonsalud.com";
+  if (redirectHosts.has(url.hostname)) {
+    url.hostname = canonicalHost;
     return Response.redirect(url.toString(), 301);
   }
 
